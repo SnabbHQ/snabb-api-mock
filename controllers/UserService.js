04d5d4380b616a7ab27e6a7f655268d04f905cbe
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-12-14 09:49:08 +0100

    Added example data to profile response

--- controllers/UserService.js
+++ controllers/UserService.js
@@ -55,13 +55,15 @@ exports.profileGET = function(args, res, next) {
    * parameters expected in the args:
   **/
     var examples = {};
-  examples['application/json'] = {
-  "thumbnail" : "aeiou",
-  "phone" : "{}",
-  "last_name" : "aeiou",
-  "first_name" : "aeiou",
-  "email" : "aeiou"
-};
+  examples['application/json'] = [ {
+  "first_name" : "Michael",
+  "last_name" : "Knight",
+  "email" : "michael.knight@snabb.io",
+  "phone" : {
+    "number" : "+46712345678"
+  },
+  "thumbnail" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTlovN715rKGVOscWvovnblMwpvwMlknTosSXthVP9xLlW7KCfw"
+} ];
   if(Object.keys(examples).length > 0) {
     res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
