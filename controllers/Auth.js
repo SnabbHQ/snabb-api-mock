bc8d3f786356e797bd5431ce06b97f53f6a19937
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-03 16:54:41 +0100

    Added really basic registration endpoint

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -9,3 +9,7 @@ var Auth = require('./AuthService');
 module.exports.authPOST = function authPOST (req, res, next) {
   Auth.authPOST(req.swagger.params, res, next);
 };
+
+module.exports.registerPOST = function registerPOST (req, res, next) {
+  Auth.registerPOST(req.swagger.params, res, next);
+};
--- controllers/AuthService.js
+++ controllers/AuthService.js
@@ -25,3 +25,28 @@ exports.authPOST = function(args, res, next) {
   
 }
 
+exports.registerPOST = function(args, res, next) {
+  /**
+   * parameters expected in the args:
+  * name (String)
+  * email (String)
+  * password (String)
+  * phone (String)
+  **/
+    var examples = {};
+  examples['application/json'] = {
+  "access_token" : "J1qK1c18UUGJFAzz9xnH56584l4",
+  "token_type" : "bearer",
+  "expires_in" : 2592000,
+  "scope" : "api"
+};
+  if(Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
+  }
+  else {
+    res.end();
+  }
+  
+}
+
